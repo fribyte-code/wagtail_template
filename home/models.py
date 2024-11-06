c6474cdf794d702e6e4f71ae3a91980716a2de2c
Kai.Waloen <kawal5842@uib.no>
2024-11-06 19:46:18 +0100

    implementing css and text template

--- home/models.py
+++ home/models.py
@@ -1,7 +1,29 @@
-from django.db import models
-
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+
 
 
 class HomePage(Page):
-    pass
+    # We use RichTextField for text fields
+    # so that the client can format the text as they like.
+
+    # Hero_text is the subtitle of the page
+    hero_text = RichTextField(
+        blank=True,
+        max_length=255, help_text="Write an introduction for the site"
+    )
+
+    # This is the body of the text
+    body = RichTextField(blank=True, help_text="Write a body for your site")
+
+    # modify your content_panels:
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("hero_text"),
+            ],
+            heading="Hero section",
+        ),
+        FieldPanel('body'),
+    ]

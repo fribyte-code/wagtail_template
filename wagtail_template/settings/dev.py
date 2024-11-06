d18b540c56259239fa7c959130c6a2bce10a3de4
Kai.Waloen <kawal5842@uib.no>
2024-11-06 17:53:05 +0100

    initial commit

---
+++ wagtail_template/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-t6f)kon#_2m30ofp2#1dg#(mc$sfxu3w)ztzj@shwyb7t=mb6e"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
